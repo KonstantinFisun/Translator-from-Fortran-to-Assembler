544717ef011a0e677c128becdff0ac3b61a1d8c2
Константин Сергеевич Фисун <kostya.ficyn@gmail.com>
2022-04-22 00:16:25 +0300

    Допилил комменты

--- main.py
+++ main.py
@@ -14,36 +14,38 @@ SERVICE_WORDS = [
 
 # Операции
 OPERATIONS = ['.GT.', '.LT.', '.GE.', '.LE.', '.NE.', '.EQ.', '.NEQV.',
-              '*', '+', '-', '**', '.', '/', '=', '<', '<=', '<>', '=', '>', '>=', 'SQRT', 'AND', 'OR', '.FALSE.', '.TRUE.', 'EQV', 'NOT', '::']
+              '*', '+', '-', '**', '.', '/', '=', '<', '<=', '<>', '=', '>', '>=', 'SQRT', 'AND', 'OR', '.FALSE.',
+              '.TRUE.', 'EQV', 'NOT', '::']
 # Разделители
 SEPARATORS = ['\t', '\n', ' ', '(', ')', ',', '.', ':', ';', '[', ']']
+
 
 # Каждому token_value(лексеме) ставит в соответсвие класс+код
 def check(tokens, token_class, token_value):
     if not (token_value in tokens[token_class]):
-        token_code = str(len(tokens[token_class]) + 1) # Код токена
-        tokens[token_class][token_value] = token_class + token_code
+        token_code = str(len(tokens[token_class]) + 1)  # Код токена
+        tokens[token_class][token_value] = token_class + token_code  # Класс+код
 
 
 # Получаем операцию
 def get_operation(input_sequence, i):
-    for k in range(3, 0, -1): # Смотрим все операции от 3 символов и до 1
-        if i + k < len(input_sequence): # Проверяем, чтобы не был конец файла
-            buffer = input_sequence[i:i + k] # Помещаем буффер наши цепочку символов
-            if buffer in OPERATIONS: # Возвращаем, если это операция
+    for k in range(3, 0, -1):  # Смотрим все операции от 3 символов и до 1
+        if i + k < len(input_sequence):  # Проверяем, чтобы не был конец файла
+            buffer = input_sequence[i:i + k]  # Помещаем буффер наши цепочку символов
+            if buffer in OPERATIONS:  # Возвращаем, если это операция
                 return buffer
     return ''
 
+
 # Получаем разделитель
 def get_separator(input_sequence, i):
-    buffer = input_sequence[i] # Записываем в буффер
-    if buffer in SEPARATORS: # Если есть, то возвращаем разделитель
+    buffer = input_sequence[i]  # Записываем в буффер
+    if buffer in SEPARATORS:  # Если есть, то возвращаем разделитель
         return buffer
     return ''
 
 
 def main():
-
     # лексемы
     # I класс идентификаторов;
     # W класс служебных слов;
@@ -64,23 +66,23 @@ def main():
 
     # файл, содержащий текст на входном языке программирования
     f = open('quadratic equation.txt', 'r')
-    input_sequence = f.read() # Считываем файл
-    f.close() # Закрываем
+    input_sequence = f.read()  # Считываем файл
+    f.close()  # Закрываем
 
-    i = 0 # Переменная для итерации
-    state = 'S' # Начальное состояние
-    output_sequence = buffer = '' # Выход
+    i = 0  # Переменная для итерации
+    state = 'S'  # Начальное состояние
+    output_sequence = buffer = ''  # Выход
 
     # Пока есть символы
     while i < len(input_sequence):
-        symbol = input_sequence[i] # Текущий символ
+        symbol = input_sequence[i]  # Текущий символ
         operation = get_operation(input_sequence, i)
         separator = get_separator(input_sequence, i)
 
         # Если начальное состояние S
         if state == 'S':
 
-            buffer = '' # Буфер
+            buffer = ''  # Буфер
 
             # Если встретили букву
             if symbol.isalpha():
@@ -103,34 +105,34 @@ def main():
 
             # Если встретили операцию
             elif operation:
-                check(tokens, 'O', operation) # Добавили операцию если её нет
-                output_sequence += tokens['O'][operation] + ' ' # Добавили в выходную последовательность
-                i += len(operation) - 1 # Перешли к след символу
+                check(tokens, 'O', operation)  # Добавили операцию если её нет
+                output_sequence += tokens['O'][operation] + ' '  # Добавили в выходную последовательность
+                i += len(operation) - 1  # Перешли к след символу
 
             # Если встретили разделитель
             elif separator:
-                if separator != ' ': # Если это не пробел
-                    check(tokens, 'R', separator) # Добавили разделитель если его нет
-                    output_sequence += tokens['R'][separator] # Добавили в выходную последовательность
-                    if separator == '\n': # Если новая строка
+                if separator != ' ':  # Если это не пробел
+                    check(tokens, 'R', separator)  # Добавили разделитель если его нет
+                    output_sequence += tokens['R'][separator]  # Добавили в выходную последовательность
+                    if separator == '\n':  # Если новая строка
                         output_sequence += '\n'
                     else:
-                        output_sequence += ' ' # Иначе пробел
+                        output_sequence += ' '  # Иначе пробел
 
             # Если конец файла
             elif i == len(input_sequence) - 1:
-                state = 'Z' # Конечное состояние
+                state = 'Z'  # Конечное состояние
 
         # Если начальное состояние q1
         elif state == 'q1':
             # Если опять символ
             if symbol.isalpha():
-                buffer += symbol # Добавили в буффер
+                buffer += symbol  # Добавили в буффер
 
             # Если встретили цифру, то это идентификатор
             elif symbol.isdigit():
-                state = 'q2' # перешли в новое состояние
-                buffer += symbol # Добавили в буффер
+                state = 'q2'  # перешли в новое состояние
+                buffer += symbol  # Добавили в буффер
 
             # Если это не цифра и не буква
             else:
@@ -144,8 +146,8 @@ def main():
 
                     # Семантическая процедура 2
                     else:
-                        check(tokens, 'I', buffer) # Проверили встречался ли раньше идентификатор
-                        output_sequence += tokens['I'][buffer] + ' ' # Добавили в выходную последовательность
+                        check(tokens, 'I', buffer)  # Проверили встречался ли раньше идентификатор
+                        output_sequence += tokens['I'][buffer] + ' '  # Добавили в выходную последовательность
 
                     # Если встретили операцию
                     if operation:
@@ -162,176 +164,174 @@ def main():
                                 output_sequence += '\n'
                             else:
                                 output_sequence += ' '  # Иначе пробел
-                state = 'S' # перешли в начальное состояние
+                state = 'S'  # перешли в начальное состояние
 
         # Если начальное состояние q2
         elif state == 'q2':
+            # Если буква или цифра
             if symbol.isalnum():
-                buffer += symbol
+                buffer += symbol  # Добавили в буффер
             else:
+                # Семантическая процедура 2
                 if operation or separator:
-                    check(tokens, 'I', buffer)
-                    output_sequence += tokens['I'][buffer] + ' '
+                    check(tokens, 'I', buffer)  # Проверили встречался ли раньше идентификатор
+                    output_sequence += tokens['I'][buffer] + ' '  # Добавили в выходную последовательность
                     if operation:
-                        check(tokens, 'O', operation)
-                        output_sequence += tokens['O'][operation] + ' '
-                        i += len(operation) - 1
+                        check(tokens, 'O', operation)  # Добавили операцию если её нет
+                        output_sequence += tokens['O'][operation] + ' '  # Добавили в выходную последовательность
+                        i += len(operation) - 1  # Перешли к след символу
                     if separator:
-                        if separator != ' ':
-                            check(tokens, 'R', separator)
-                            output_sequence += tokens['R'][separator]
-                            if separator == '\n':
+                        if separator != ' ':  # Если это не пробел
+                            check(tokens, 'R', separator)  # Добавили разделитель если его нет
+                            output_sequence += tokens['R'][separator]  # Добавили в выходную последовательность
+                            if separator == '\n':  # Если новая строка
                                 output_sequence += '\n'
                             else:
-                                output_sequence += ' '
-                    state = 'S'
+                                output_sequence += ' '  # Иначе пробел
+                    state = 'S'  # Переход в начальное состояние
 
         # Если начальное состояние q3
         elif state == 'q3':
+            # Если цифра
             if symbol.isdigit():
-                buffer += symbol
+                buffer += symbol  # Добавили в буффер
+            # Если точка
             elif symbol == '.':
-                state = 'q4'
-                buffer += symbol
+                state = 'q4'  # Перешли в новое состояние
+                buffer += symbol  # Добавили в буффер
+            # Если e или E
             elif symbol == 'e' or symbol == 'E':
-                state = 'q6'
-                buffer += symbol
+                state = 'q6'  # Перешли в новое состояние
+                buffer += symbol  # Добавили в буффер
             else:
+                # Семантическая процедура 3
                 if operation or separator:
-                    check(tokens, 'N', buffer)
-                    output_sequence += tokens['N'][buffer] + ' '
+                    check(tokens, 'N', buffer)  # Занесли числовую константу
+                    output_sequence += tokens['N'][buffer] + ' '  # Добавили в выходную последовательность
                     if operation:
-                        check(tokens, 'O', operation)
-                        output_sequence += tokens['O'][operation] + ' '
-                        i += len(operation) - 1
+                        check(tokens, 'O', operation)  # Добавили операцию если её нет
+                        output_sequence += tokens['O'][operation] + ' '  # Добавили в выходную последовательность
+                        i += len(operation) - 1  # Перешли к след символу
                     if separator:
-                        if separator != ' ':
-                            check(tokens, 'R', separator)
-                            output_sequence += tokens['R'][separator]
-                            if separator == '\n':
+                        if separator != ' ':  # Если это не пробел
+                            check(tokens, 'R', separator)  # Добавили разделитель если его нет
+                            output_sequence += tokens['R'][separator]  # Добавили в выходную последовательность
+                            if separator == '\n':  # Если новая строка
                                 output_sequence += '\n'
                             else:
-                                output_sequence += ' '
-                    state = 'S'
+                                output_sequence += ' '  # Иначе пробел
+                    state = 'S'  # Переход в начальное состояние
 
         # Если начальное состояние q4
         elif state == 'q4':
+            # Проверили, что цифра
             if symbol.isdigit():
-                state = 'q5'
-                buffer += symbol
+                state = 'q5'  # Перешли в новое состояние
+                buffer += symbol  # Добавили в буффер
 
         # Если начальное состояние q5
         elif state == 'q5':
+            # Если цифра
             if symbol.isdigit():
-                buffer += symbol
+                buffer += symbol  # Добавили в буффер
+            # Если e или E
             elif symbol == 'e' or symbol == 'E':
-                state = 'q6'
-                buffer += symbol
+                state = 'q6'  # Перешли в новое состояние
+                buffer += symbol  # Добавили в буффер
             else:
+                # Семантическая процедура 3
                 if operation or separator:
-                    check(tokens, 'N', buffer)
-                    output_sequence += tokens['N'][buffer] + ' '
+                    check(tokens, 'N', buffer)  # Занесли числовую константу
+                    output_sequence += tokens['N'][buffer] + ' '  # Добавили в выходную последовательность
                     if operation:
-                        check(tokens, 'O', operation)
-                        output_sequence += tokens['O'][operation] + ' '
-                        i += len(operation) - 1
+                        check(tokens, 'O', operation)  # Добавили операцию если её нет
+                        output_sequence += tokens['O'][operation] + ' '  # Добавили в выходную последовательность
+                        i += len(operation) - 1  # Перешли к след символу
                     if separator:
-                        if separator != ' ':
-                            check(tokens, 'R', separator)
-                            output_sequence += tokens['R'][separator]
-                            if separator == '\n':
+                        if separator != ' ':  # Если это не пробел
+                            check(tokens, 'R', separator)  # Добавили разделитель если его нет
+                            output_sequence += tokens['R'][separator]  # Добавили в выходную последовательность
+                            if separator == '\n':  # Если новая строка
                                 output_sequence += '\n'
                             else:
-                                output_sequence += ' '
-                    state = 'S'
+                                output_sequence += ' '  # Иначе пробел
+                    state = 'S'  # Перешли в начальное состояние
 
         # Если начальное состояние q6
         elif state == 'q6':
+            # Если - или +
             if symbol == '-' or symbol == '+':
-                state = 'q7'
-                buffer += symbol
+                state = 'q7'  # Перешли в новое состояние
+                buffer += symbol  # Добавили в буффер
+            # Если цифра
             elif symbol.isdigit():
-                state = 'q8'
-                buffer += symbol
+                state = 'q8'  # Перешли в новое состояние
+                buffer += symbol  # Добавили в буффер
 
         # Если начальное состояние q7
         elif state == 'q7':
+            # Если цифра
             if symbol.isdigit():
-                state = 'q8'
-                buffer += symbol
+                state = 'q8'  # Перешли в новое состояние
+                buffer += symbol  # Добавили в буффер
 
         # Если начальное состояние q8
         elif state == 'q8':
+            # Если цифра
             if symbol.isdigit():
-                buffer += symbol
+                buffer += symbol  # Добавили в буффер
             else:
+                # Семантическая процедура 3
                 if operation or separator:
-                    check(tokens, 'N', buffer)
-                    output_sequence += tokens['N'][buffer] + ' '
+                    check(tokens, 'N', buffer)  # Занесли числовую константу
+                    output_sequence += tokens['N'][buffer] + ' '  # Добавили в выходную последовательность
                     if operation:
-                        check(tokens, 'O', operation)
-                        output_sequence += tokens['O'][operation] + ' '
-                        i += len(operation) - 1
+                        check(tokens, 'O', operation)  # Добавили операцию если её нет
+                        output_sequence += tokens['O'][operation] + ' '  # Добавили в выходную последовательность
+                        i += len(operation) - 1  # Перешли к след символу
                     if separator:
-                        if separator != ' ':
-                            check(tokens, 'R', separator)
-                            output_sequence += tokens['R'][separator]
-                            if separator == '\n':
+                        if separator != ' ':  # Если это не пробел
+                            check(tokens, 'R', separator)  # Добавили разделитель если его нет
+                            output_sequence += tokens['R'][separator]  # Добавили в выходную последовательность
+                            if separator == '\n':  # Если новая строка
                                 output_sequence += '\n'
                             else:
-                                output_sequence += ' '
-                state = 'S'
+                                output_sequence += ' '  # Иначе пробел
+                state = 'S'  # Перешли в начальное состояние
 
         # Если начальное состояние q9
         elif state == 'q9':
+            # Если символ не апостороф
             if symbol != "'":
-                buffer += symbol
+                buffer += symbol  # Добавили в буффер
+            # Если символ апостроф
             elif symbol == "'":
-                buffer += symbol
-                check(tokens, 'C', buffer)
-                output_sequence += tokens['C'][buffer] + ' '
-                state = 'S'
+                buffer += symbol  # Добавили в буффер
+                check(tokens, 'C', buffer)  # Добавили символьною константу если её нет
+                output_sequence += tokens['C'][buffer] + ' '  # Добавили в выходную последовательность
+                state = 'S'  # Перешли в начальное состояние
 
         # Если начальное состояние q10
         elif state == 'q10':
-            if symbol == '/':
-                state = 'q11'
-
-        # Если начальное состояние q11
-        elif state == 'q11':
+            # Если конец строки
             if symbol == '\n':
                 state = 'S'
+            # Если конец файла
             elif i == len(input_sequence) - 1:
                 state = 'Z'
 
-        # Если начальное состояние q12
-        elif state == 'q12':
-            if symbol == '}':
-                state = 'q11'
-
-        # Если начальное состояние q13
-        elif state == 'q13':
-            if symbol == '\n':
-                state = 'S'
-            elif i == len(input_sequence) - 1:
-                state = 'Z'
-        i += 1
+        i += 1  # Переходим к следующему символу
 
     # файлы, содержащие все таблицы лексем
     for token_class in tokens.keys():
-        # if len(tokens[token_class]) > 0:
-        # f = open('%s.txt' % token_class, 'w')
-        # for token_value, token_code in tokens[token_class].items():
-        # f.write(token_code + ' ' + token_value + '\n')
-        # f.close()
         with open('%s.json' % token_class, 'w') as write_file:
             data = {val: key for key, val in tokens[token_class].items()}
             json.dump(data, write_file, indent=4)
 
     # файл, содержащий последовательность кодов лексем входной программы
-    f = open('tokens.txt', 'w')
-    f.write(output_sequence)
-    f.close()
+    f = open('tokens.txt', 'w')  # Открыли для записи
+    f.write(output_sequence)  # Записали выходную последовательность
+    f.close()  # Закрыли файл
 
 
 if __name__ == '__main__':
